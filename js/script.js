9f8b0a497bd0e3cc2bd4504909e014f75ebae8da
DonutSergius <popov.serg.2003.09@gmail.com>
2024-02-08 17:11:28 +0200

    Created Feedback

--- js/script.js
+++ js/script.js
@@ -52,3 +52,86 @@ document.addEventListener('DOMContentLoaded', function () {
 
     autoChangeImage();
 });
+
+
+
+var nameInput = document.getElementById("name");
+var phoneInput = document.getElementById("phone");
+var emailInput = document.getElementById("email");
+document.addEventListener("DOMContentLoaded", function () {
+  var feedbackContant = document.querySelector(".feedback-contant");
+  var feedbackButton = document.querySelector(".feedback-button");
+  var feedbackEnd = document.querySelector(".feedback-end");
+  var feedbackSend = document.querySelector(".feedback-send");
+
+  feedbackButton.addEventListener("click", function () {
+    feedbackContant.classList.add("fullscreen");
+  });
+
+  feedbackEnd.addEventListener("click", function () {
+    nameInput.value = "";
+    phoneInput.value = "";
+    emailInput.value = "";
+    if (nameInput.classList.contains("error")) {
+      nameInput.classList.remove("error");
+      nameInput.style.color = "";
+    }
+
+    if (phoneInput.classList.contains("error")) {
+      phoneInput.classList.remove("error");
+      phoneInput.style.color = "";
+    }
+
+    if (emailInput.classList.contains("error")) {
+      emailInput.classList.remove("error");
+      emailInput.style.color = "";
+    }
+    feedbackContant.classList.remove("fullscreen");
+  });
+
+  feedbackSend.addEventListener("click", function () {
+    if (validateForm()) {
+      feedbackContant.classList.remove("fullscreen");
+    }
+  });
+
+  nameInput.addEventListener("focus", function () {
+    removeError(nameInput);
+  });
+
+  phoneInput.addEventListener("focus", function () {
+    removeError(phoneInput);
+  });
+
+  emailInput.addEventListener("focus", function () {
+    removeError(emailInput);
+  });
+});
+function validateForm() {
+  var isNameValid = validateInput(nameInput);
+  var isPhoneValid = validateInput(phoneInput);
+  var isEmailValid = validateInput(emailInput);
+
+  return isNameValid && isPhoneValid && isEmailValid;
+}
+
+function validateInput(inputElement) {
+  var value = inputElement.value.trim();
+
+  if (value === "" || value === "This field is required.") {
+    inputElement.classList.add("error");
+    inputElement.style.color = "red";
+    inputElement.value = "This field is required.";
+    return false;
+  } else {
+    inputElement.classList.remove("error");
+    inputElement.style.color = "";
+
+    return true;
+  }
+}
+
+function removeError(inputElement) {
+  inputElement.classList.remove("error");
+  inputElement.style.color = "";
+}
